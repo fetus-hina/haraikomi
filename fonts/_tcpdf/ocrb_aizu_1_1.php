c3e57e08933c8193983a42763944c5afd60dceca
AIZAWA Hina <hina@bouhime.com>
2017-09-25 23:01:49 +0900

    Add fonts data for TCPDF

---
+++ fonts/_tcpdf/ocrb_aizu_1_1.php
@@ -0,0 +1,15 @@
+<?php
+// TCPDF FONT FILE DESCRIPTION
+$type='TrueTypeUnicode';
+$name='OCRB';
+$up=63;
+$ut=84;
+$dw=602;
+$diff='';
+$originalsize=3712;
+$enc='';
+$file='ocrb_aizu_1_1.z';
+$ctg='ocrb_aizu_1_1.ctg.z';
+$desc=array('Flags'=>33,'FontBBox'=>'[33 0 540 717]','ItalicAngle'=>0,'Ascent'=>712,'Descent'=>0,'Leading'=>555,'CapHeight'=>667,'XHeight'=>667,'StemV'=>34,'StemH'=>15,'AvgWidth'=>602,'MaxWidth'=>602,'MissingWidth'=>602);
+$cw=array(0=>602,48=>602,49=>602,50=>602,51=>602,52=>602,53=>602,54=>602,55=>602,56=>602,57=>602,65535=>602);
+// --- EOF ---
