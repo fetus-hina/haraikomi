7510971734b54b5b773882b84a26dee218d733c2
AIZAWA Hina <hina@bouhime.com>
2017-09-30 21:26:04 +0900

    Switch env

--- web/index.php
+++ web/index.php
@@ -1,9 +1,10 @@
 <?php
 define('K_PATH_FONTS', __DIR__ . '/../resources/fonts/_tcpdf/');
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+if (!file_exists(__DIR__ . '/../.production')) {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+}
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
