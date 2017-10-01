ac59940aed5635dc6606b6e07dec4e0a0b9b70a7
AIZAWA Hina <hina@bouhime.com>
2017-10-01 20:55:57 +0900

    Update layout

--- views/site/index.php
+++ views/site/index.php
@@ -9,14 +9,15 @@ $this->title = Yii::$app->name;
   <p>
     ゆうちょ銀行の「払込取扱票」に印刷するためのPDFを作成するサイトです。<br>
     「払込取扱票」はゆうちょ銀行においてあるこんな紙です。ATMとかにあります。わからなければ窓口で聞けばくれるはずです。<br>
-    <img src="/images/haraikomi.png"><br>
+    <img src="/images/haraikomi.png" class="img-fluid"><br>
     自分で全部印刷するようなことは禁止されているため、ゆうちょ銀行で紙を取ってきて、そこに「手書きの代わりに」印刷する必要があります。
   </p>
   <p>
-    入力されたデータについては一切保存していません。（<a href="https://github.com/fetus-hina/haraikomi">ソース</a>）
+    入力されたデータについては一切保存していません。
+    （<a href="https://github.com/fetus-hina/haraikomi">ソースコード</a>）
   </p>
   <p>
-    作成されるPDFは、払込取扱票に合わせた特殊な用紙サイズです。<br>
+    作成されるPDFは、払込取扱票に合わせた特殊な用紙サイズです。(180mm×114mm)<br>
     これをうまく扱えるプリンタをうまく設定して使う必要があります。場合によっては結構難しいです。<br>
     手書きで一文字も書きたくないような人を除いて手書きしたほうが圧倒的にはやくて楽です。
   </p>
@@ -27,18 +28,18 @@ $this->title = Yii::$app->name;
       <div class="col-12 col-lg-6">
         <label>記号・番号</label>
         <div class="row">
-          <div class="col-5">
+          <div class="col-4 col-md-5">
             <?= $_->field($form, 'account1')
               ->label(false)
               ->textInput(['placeholder' => '012345']) . "\n"
             ?>
           </div>
-          <div class="col-2">
+          <div class="col-4 col-md-2">
             <?= $_->field($form, 'account2')
               ->label(false)
               ->textInput(['placeholder' => '0']) . "\n" ?>
           </div>
-          <div class="col-5">
+          <div class="col-4 col-md-5">
             <?= $_->field($form, 'account3')
               ->label(false)
               ->textInput(['placeholder' => '98765']) . "\n" ?>
@@ -70,8 +71,8 @@ $this->title = Yii::$app->name;
     <div class="form-group">
       <label>電話番号</label>
       <div class="form-inline">
-        <?= $_->field($form, 'phone1')->label(false)->textInput(['placeholder' => '090', 'size' => 5]) ?> -
-        <?= $_->field($form, 'phone2')->label(false)->textInput(['placeholder' => '1234', 'size' => 5]) ?> -
+        <?= $_->field($form, 'phone1')->label(false)->textInput(['placeholder' => '090', 'size' => 5]) . "\n" ?>
+        <?= $_->field($form, 'phone2')->label(false)->textInput(['placeholder' => '1234', 'size' => 5]) . "\n" ?>
         <?= $_->field($form, 'phone3')->label(false)->textInput(['placeholder' => '5678', 'size' => 5]) . "\n" ?>
       </div>
     </div>
