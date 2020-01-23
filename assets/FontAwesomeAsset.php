7c5d6226d7fcbefa7832e3558cbcf5a780d71b39
AIZAWA Hina <hina@fetus.jp>
2020-01-23 17:42:39 +0900

    upgrade FontAwesome

--- assets/FontAwesomeAsset.php
+++ assets/FontAwesomeAsset.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace app\assets;
 
 use yii\web\AssetBundle;
@@ -10,6 +12,6 @@ class FontAwesomeAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [];
     public $js = [
-        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-11/js/all.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-1/js/all.min.js',
     ];
 }
