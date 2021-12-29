504e5aec67b38ab0fc9e4e3f058f325d0244dc9e
AIZAWA Hina <hina@fetus.jp>
2021-12-29 11:21:12 +0900

    Renew sample image

--- widgets/SampleImageWidget.php
+++ widgets/SampleImageWidget.php
@@ -4,31 +4,66 @@ declare(strict_types=1);
 
 namespace app\widgets;
 
-use Yii;
-use app\assets\SampleImageAsset;
 use yii\base\Widget;
 use yii\bootstrap5\BootstrapAsset;
 use yii\bootstrap5\Html;
-use yii\helpers\Json;
 
 final class SampleImageWidget extends Widget
 {
+    private const WIDTH = 609;
+    private const HEIGHT = 384;
+
     public function run(): string
     {
-        SampleImageAsset::register($this->view);
         BootstrapAsset::register($this->view);
+        $id = $this->id;
 
-        $this->view->registerJs(vsprintf('$(%s).sampleImage();', [
-            Json::encode(sprintf('#%s', $this->id)),
+        // https://easings.net/#easeInOutQuart
+        $easeInOutQuart = 'cubic-bezier(0.76, 0, 0.24, 1)';
+        $this->view->registerCss($this->renderCss([
+            "#{$id}" => [
+                '--bs-aspect-ratio' => sprintf('%.9f%%', self::HEIGHT / self::WIDTH * 100),
+                'max-width' => sprintf('%dpx', self::WIDTH),
+            ],
+            "#{$id}-sample" => [
+                'opacity' => '0',
+                'transition' => "opacity 0.25s {$easeInOutQuart} 0s",
+            ],
+            "#{$id}-sample:hover" => [
+                'opacity' => '1',
+            ],
         ]));
 
-        return Html::img('@web/images/haraikomi.png', [
-            'class' => 'img-fluid',
-            'data' => [
-                'original' => Yii::getAlias('@web/images/haraikomi.png'),
-                'hover' => Yii::getAlias('@web/images/haraikomi-sample.png'),
-            ],
-            'id' => $this->id,
-        ]);
+        return Html::tag(
+            'div',
+            implode('', [
+                Html::img('@web/images/haraikomi.png', [
+                    'alt' => '',
+                    'title' => '',
+                ]),
+                Html::img('@web/images/sample.png', [
+                    'alt' => '',
+                    'id' => sprintf('%s-sample', $id),
+                    'title' => '',
+                ]),
+            ]),
+            [
+                'class' => 'ratio',
+                'id' => $id,
+            ]
+        );
+    }
+
+    /** @param array<string, array<string, string>> $css */
+    private function renderCss(array $css): string
+    {
+        return implode('', array_map(
+            fn (string $selector, array $css): string => vsprintf('%s{%s}', [
+                $selector,
+                Html::cssStyleFromArray($css),
+            ]),
+            array_keys($css),
+            array_values($css),
+        ));
     }
 }
