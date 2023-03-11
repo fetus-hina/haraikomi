c923fc17342332227c1b129c3d7842787ffa1bb7
AIZAWA Hina <hina@fetus.jp>
2023-03-12 02:36:06 +0900

    Update asset URL

--- config/asset-manager.php
+++ config/asset-manager.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use ParagonIE\ConstantTime\Base32;
 use yii\bootstrap5\BootstrapAsset;
 use yii\bootstrap5\BootstrapPluginAsset;
 use yii\validators\PunycodeAsset;
@@ -11,7 +12,7 @@ use yii\widgets\PjaxAsset;
 
 return [
     'linkAssets' => true,
-    'appendTimestamp' => true,
+    'appendTimestamp' => false,
     'bundles' => [
         BootstrapAsset::class => [
             'sourcePath' => '@npm/@fetus-hina/fetus.css/dist',
@@ -41,4 +42,35 @@ return [
             'sourcePath' => '@npm/yii2-pjax',
         ],
     ],
+    'hashCallback' => function (string $path): string {
+        $pathParts = [];
+
+        static $appPath = null;
+        if (!$appPath) {
+            $appPath = realpath((string)Yii::getAlias('@app'));
+        }
+        if ($appPath) {
+            $pathParts[] = substr(
+                Base32::encodeUnpadded(
+                    hash('sha256', $appPath, true),
+                ),
+                0,
+                8,
+            );
+        }
+
+        $pathParts[] = substr(
+            Base32::encodeUnpadded(
+                hash(
+                    'sha256',
+                    is_file($path) ? dirname($path) : $path,
+                    true,
+                ),
+            ),
+            0,
+            8,
+        );
+
+        return implode('/', $pathParts);
+    },
 ];
