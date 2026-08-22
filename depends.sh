627e20110befb57379df350a8cd1bd4557bb4824
AIZAWA Hina <hina@fetus.jp>
2026-08-22 09:52:31 +0900

    Update dependencies

--- depends.sh
+++ depends.sh
@@ -3,8 +3,9 @@
 npx updates -u -m
 \rm -rf package-lock.json node_modules
 npm install
-npm dedupe
 
 make composer.phar
 ./composer.phar self-update
-./composer.phar update
+./composer.phar update -W
+./composer.phar bump
+./composer.phar normalize
